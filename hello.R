72950dfb259322bbca70c3dd66730e0d5a6bc451
laura-green <laurange.green@gmail.com>
2016-09-14 09:03:25 +0200

    Few modifs

--- hello.R
+++ hello.R
@@ -1,1 +1,7 @@
+# Origin: Hello World
+# Filename: hello.R
+# Author: Laura Green
+# Last modified: 14 September, 2016
+# This is a basic function to learn about programming in R. 
+# It prints "Hello world!".
 print ("Hello world!", quote=FALSE)
