27606dc64cdc3fb611258c87cbcea828126c2e2f
laura-green <laurange.green@gmail.com>
2016-09-14 09:40:43 +0200

    trying to solve problem

--- README.md
+++
@@ -1,2 +0,0 @@
-# RepoLauraProgramming
-This is my first repo
--- hello.R
+++
@@ -1,7 +0,0 @@
-# Origin: Hello World
-# Filename: hello.R
-# Author: Laura Green
-# Last modified: 14 September, 2016
-# This is a basic function to learn about programming in R. 
-# It prints "Hello world!".
-print ("Hello world!", quote=FALSE)
